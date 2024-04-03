56bc0abebcb2c95d1b2474d1334ba509f6725046
Zilvinas <40381571+ZVince9@users.noreply.github.com>
2024-04-03 23:02:07 +0300

    test

---
+++ py-tests/tests_ui/pages/login_module.py
@@ -0,0 +1,38 @@
+from playwright.sync_api import Page
+
+class Login:
+    def __init__(self, page: Page, url):
+        self._page = page
+        self._url = url
+        self._username = 'standard_user'
+        self._password = 'secret_sauce'
+
+    @property
+    def login_page(self):
+        return self._url
+
+    @login_page.setter
+    def login_page(self, new_url):
+        if not isinstance(new_url, str):
+            raise ValueError("Inserted URL is not a string")
+        self._url = new_url
+
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, new_username):
+        if not isinstance(new_username, str):
+            raise ValueError("Username must be a string")
+        self._username = new_username
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, new_password):
+        if not isinstance(new_password, str):
+            raise ValueError("Password must be a string")
+        self._password = new_password
---
+++ py-tests/tests_ui/test_login.py
@@ -0,0 +1,66 @@
+import re
+
+import pytest
+
+from playwright.sync_api import Page, expect
+
+from helpers.variables import main_url
+
+from pages.login_module import Login
+
+
+@pytest.fixture
+def login(page: Page):
+    login = Login(page, main_url)
+    return login
+
+
+def test_login_to_page_title(page: Page, login):
+
+    page.goto(login.login_page)
+
+    # # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Swag Labs"))
+
+
+def test_login_to_new_page_title(page: Page, login):
+    login.login_page = 'https://google.com'
+
+    page.goto(login.login_page)
+
+    # # Expect a title "to contain" a new substring.
+    expect(page).to_have_title(re.compile("Google"))
+
+
+# you can run this test using mark
+# pytest -m login -v
+@pytest.mark.login
+def test_sign_up_to_page_with_standard_user(page: Page, login):
+    page.goto(login.login_page)
+
+    page.fill('input[data-test="username"]', login.username)
+    page.fill('input[data-test="password"]', login.password)
+
+    page.click('input[data-test="login-button"]')
+
+    expect(page.locator('div[data-test="primary-header"]')).to_be_visible()
+
+
+# you can run this test using mark
+# pytest -m login_locked -v
+@pytest.mark.login_locked
+def test_sign_up_to_page_with_locked_user(page: Page, login):
+    page.goto(login.login_page)
+    login.username = "locked_out_user"
+
+    page.fill('input[data-test="username"]', login.username)
+    page.fill('input[data-test="password"]', login.password)
+
+    page.click('input[data-test="login-button"]')
+
+    expect(page.locator('button[data-test="error-button"]')).to_be_visible()
+
+
+
+
+# test
